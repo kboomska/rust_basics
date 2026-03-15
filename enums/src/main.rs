bb1d04468ff85782c6e23c74734b132298d29b06
Aleksey Kuzmin <kboomska@gmail.com>
2026-03-15 12:03:48 +0300

    Прикрепление значений перечисления для каждого варианта перечисления.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,33 +1,10 @@
 // Перечисление типов IP адресов.
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-// Хранение значения IP адреса вместе с его типом с использованием структур.
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    // Значения перечислений.
-
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
-    route(four);
-    route(six);
-
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
 }
-
-fn route(ip_kind: IpAddrKind) {}
