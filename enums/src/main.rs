069484c288424b62a274d8b0b3734d79c6fc2b2c
Aleksey Kuzmin <kboomska@gmail.com>
2026-03-15 11:58:17 +0300

    Использование структур для хранения значений перечисления.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -4,6 +4,12 @@ enum IpAddrKind {
     V6,
 }
 
+// Хранение значения IP адреса вместе с его типом с использованием структур.
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
     // Значения перечислений.
 
@@ -12,6 +18,16 @@ fn main() {
 
     route(four);
     route(six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
 
 fn route(ip_kind: IpAddrKind) {}
