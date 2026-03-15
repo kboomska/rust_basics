a892c28225c746a390b0e84b0d97a40e3765b48d
Aleksey Kuzmin <kboomska@gmail.com>
2026-03-15 15:05:51 +0300

    Сопоставление шаблона для Option<T>.

--- pattern_matching/src/main.rs
+++ pattern_matching/src/main.rs
@@ -14,6 +14,10 @@ enum UsState {
 
 fn main() {
     value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -28,5 +32,12 @@ fn value_in_cents(coin: Coin) -> u8 {
             println!("State quarter from {state:?}!");
             25
         }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
