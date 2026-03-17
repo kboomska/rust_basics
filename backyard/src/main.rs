327591282d8688e4b4524e1e736daf09140386b1
Aleksey Kuzmin <kboomska@gmail.com>
2026-03-17 22:37:52 +0300

    Поясняющие комментарии для подключаемых модулей.

--- backyard/src/main.rs
+++ backyard/src/main.rs
@@ -1,5 +1,10 @@
+// main.rs - корневой модуль бинарного крейта.
+
+// Создание псевдонима для пути к типу Asparagus в подмодуле vegetables
+// модуля garden.
 use crate::garden::vegetables::Asparagus;
 
+// Подключение общедоступного модуля garden.
 pub mod garden;
 
 fn main() {
