636e46ad95d3920c02d53263b6c1441b9f40beab
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-03-20 15:45:27 +0300

    Общедоступные структуры и перечисления.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -3,43 +3,82 @@
 // Содержимое любого из файлов main.rs или lib.rs образует неявный модуль
 // crate в корне структуры модулей крейта, известной как дерево модулей.
 
-mod front_of_house {
-    // Публичный модуль.
-    pub mod hosting {
-        // Публичный метод.
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    // Модуль приватный (по умолчанию).
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-// Два способа вызова функции add_to_waitlist.
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
-}
-
-// Относительный путь с использованием super.
-
-fn deliver_order() {}
+// Общедоступные структуры и перечисления.
 
 mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    fn cook_order() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // Все варианты публичного перечисления также публичны.
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal.
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+// mod front_of_house {
+//     // Публичный модуль.
+//     pub mod hosting {
+//         // Публичный метод.
+//         pub fn add_to_waitlist() {}
+
+//         fn seat_at_table() {}
+//     }
+
+//     // Модуль приватный (по умолчанию).
+//     mod serving {
+//         fn take_order() {}
+
+//         fn serve_order() {}
+
+//         fn take_payment() {}
+//     }
+// }
+
+// // Два способа вызова функции add_to_waitlist.
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+// }
+
+// // Относительный путь с использованием super.
+
+// fn deliver_order() {}
+
+// mod back_of_house {
+//     fn fix_incorrect_order() {
+//         cook_order();
+//         super::deliver_order();
+//     }
+
+//     fn cook_order() {}
+// }
