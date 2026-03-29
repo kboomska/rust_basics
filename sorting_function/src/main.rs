afe6a8f06b180e021649f203efd56cf17192c6c4
Aleksey Kuzmin <kboomska@gmail.com>
2026-03-30 00:01:38 +0300

    Определение медианы и моды для вектора.

--- sorting_function/src/main.rs
+++ sorting_function/src/main.rs
@@ -1,12 +1,22 @@
+use std::collections::HashMap;
+
 use rand::Rng;
 
 fn main() {
     let v = generate_vector();
-    println!("Vector: {:?}", v);
+    println!("Vector: {v:?}");
 
-    // Среднее значение списка.
+    // Среднее значение вектора.
     let mean = vector_mean(&v);
     println!("Mean: {mean}");
+
+    // Медианное значение вектора.
+    let median = vector_median(&v);
+    println!("Median: {median}");
+
+    // Мода вектора.
+    let mode = vector_mode(&v);
+    println!("Mode: {mode}");
 }
 
 /// Генерация исходного вектора значений.
@@ -31,4 +41,32 @@ fn vector_mean(vector: &[i32]) -> f64 {
     let sum: i32 = vector.iter().sum();
 
     sum as f64 / length as f64
+}
+
+/// Вычисление медианы для вектора.
+fn vector_median(vector: &[i32]) -> i32 {
+    let mut v = vector.to_vec(); // Клонирование вектора.
+    v.sort();
+    v[v.len() / 2]
+}
+
+/// Вычисление моды вектора.
+fn vector_mode(vector: &[i32]) -> i32 {
+    let mut map = HashMap::new();
+    let mut mode = vector[0];
+    let mut biggest_count = 0;
+
+    for value in vector {
+        let count = map.entry(value).or_insert(0);
+        *count += 1;
+    }
+
+    for (key, value) in map {
+        if value > biggest_count {
+            mode = *key;
+            biggest_count = value;
+        }
+    }
+
+    mode
 }
