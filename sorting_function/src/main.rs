67c81efa964c8493319756bc9bc0e03ce652b1ed
Aleksey Kuzmin <kboomska@gmail.com>
2026-03-29 22:39:26 +0300

    Вычисление среднего значения для вектора целых чисел.

---
+++ sorting_function/src/main.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+
+fn main() {
+    let v = generate_vector();
+    println!("{:?}", v);
+
+    // Среднее значение списка.
+    let mean = vector_mean(&v);
+    println!("Mean: {mean}");
+}
+
+/// Генерация исходного вектора значений.
+fn generate_vector() -> Vec<i32> {
+    let mut rng = rand::thread_rng();
+    let length = rng.gen_range(5..=10);
+    let mut vector = vec![0; length];
+
+    for index in 0..vector.len() {
+        let value = rng.gen_range(1..=5);
+        vector[index] = value;
+    }
+
+    vector
+}
+
+/// Вычисление среднего значения для вектора.
+fn vector_mean(vector: &Vec<i32>) -> f64 {
+    let length = vector.len();
+
+    // Сложение всех значений в цикле.
+    // let mut sum = 0;
+    // for value in vector {
+    //     sum += value;
+    // }
+
+    // Сумма всех значений используя метод sum для Iter.
+    let sum: i32 = vector.iter().sum();
+
+    sum as f64 / length as f64
+}
