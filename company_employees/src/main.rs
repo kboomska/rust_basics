b5441323da1ca9906204e906bd04cd3caa69f33d
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-03 16:45:43 +0300

    Вывод сортированного списка сотрудников по отделам.

--- company_employees/src/main.rs
+++ company_employees/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut company: HashMap<String, Vec<String>> = HashMap::new();
 
     println!("Enter \"Add <Name> to <Department>\" to add an employee");
+    println!("Enter \"Employees <Department>\" to get a list of department employees");
     println!("Enter \"exit\" to close the app");
 
     loop {
@@ -11,6 +12,8 @@ fn main() {
 
         // Add Sally to Engineering
         // Add Amir to Sales
+        // Add Max to Sales
+        // Add Lisa to Sales
         io::stdin()
             .read_line(&mut raw_input)
             .expect("Failed to read line");
@@ -28,8 +31,13 @@ fn main() {
         match command {
             None => continue,
             Some("Add") => {
-                if let Some(rest) = input.strip_prefix("Add") {
+                if let Some(rest) = input.strip_prefix("Add ") {
                     add_employee(&mut company, rest.trim());
+                }
+            }
+            Some("Employees") => {
+                if let Some(rest) = input.strip_prefix("Employees ") {
+                    get_employees(&company, rest);
                 }
             }
             Some(_) => {
@@ -53,4 +61,18 @@ fn add_employee(company: &mut HashMap<String, Vec<String>>, employee: &str) {
     let department = parts[1].trim().to_string();
 
     company.entry(department).or_default().push(name);
+}
+
+fn get_employees(company: &HashMap<String, Vec<String>>, department: &str) {
+    match company.get(department) {
+        Some(employees) => {
+            println!("Employees in {department}:");
+            let mut sorted = employees.clone();
+            sorted.sort();
+            for employee in &sorted {
+                println!("  - {employee}");
+            }
+        }
+        None => println!("The company does not have a {department} department."),
+    }
 }
