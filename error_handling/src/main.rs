71c1e1686437b8c2b42a04a217a8892e1d258c3e
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-10 12:44:10 +0300

    Сокращение для проброса ошибок: оператор "?".

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     // Исправимые ошибки с Result
 
-    use std::fs::File;
+    use std::fs::{self, File};
     use std::io::{Error, ErrorKind, Read};
 
     // let greeting_file_result = File::open("hello.txt");
@@ -50,19 +50,42 @@ fn main() {
 
     // Проброс ошибок
 
+    // fn read_username_from_file() -> Result<String, Error> {
+    //     let username_file_result = File::open("hello.txt");
+
+    //     let mut username_file = match username_file_result {
+    //         Ok(file) => file,
+    //         Err(e) => return Err(e),
+    //     };
+
+    //     let mut username = String::new();
+
+    //     match username_file.read_to_string(&mut username) {
+    //         Ok(_) => Ok(username),
+    //         Err(e) => Err(e),
+    //     }
+    // }
+
+    // Сокращение для проброса ошибок: оператор ?
+
+    // fn read_username_from_file() -> Result<String, Error> {
+    //     let mut username_file = File::open("hello.txt")?;
+    //     let mut username = String::new();
+    //     username_file.read_to_string(&mut username)?;
+    //     Ok(username)
+    // }
+
+    // Сокращение кода за счет использования цепочки вызовов
+
+    // fn read_username_from_file() -> Result<String, Error> {
+    //     let mut username = String::new();
+    //     File::open("hello.txt")?.read_to_string(&mut username)?;
+    //     Ok(username)
+    // }
+
+    // Использование fs::read_to_string
+
     fn read_username_from_file() -> Result<String, Error> {
-        let username_file_result = File::open("hello.txt");
-
-        let mut username_file = match username_file_result {
-            Ok(file) => file,
-            Err(e) => return Err(e),
-        };
-
-        let mut username = String::new();
-
-        match username_file.read_to_string(&mut username) {
-            Ok(_) => Ok(username),
-            Err(e) => Err(e),
-        }
+        fs::read_to_string("hello.txt")
     }
 }
