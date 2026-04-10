70850bf4980a05f5d9b1d2f3593fc8441ea998d6
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-10 11:53:39 +0300

    Обработка различных ошибок с помощью match.

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -12,11 +12,20 @@ fn main() {
     // Исправимые ошибки с Result
 
     use std::fs::File;
+    use std::io::ErrorKind;
 
     let greeting_file_result = File::open("hello.txt");
 
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {error:?}"),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            _ => {
+                panic!("Problem opening the file: {error:?}");
+            }
+        },
     };
 }
