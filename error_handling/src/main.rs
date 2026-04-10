02a6191f7d580028670812315bc0598edf5f00ef
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-10 11:35:20 +0300

    Использование обратной трассировки panic!.

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     // Неустранимые ошибки с макросом panic!
 
-    panic!("crash and burn");
+    // panic!("crash and burn");
+
+    // Использование обратной трассировки panic!
+
+    let v = vec![1, 2, 3];
+
+    v[99];
 }
