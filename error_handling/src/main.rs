d048e2d0c24b57107002d2df90c74b3d10a845ed
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-10 11:57:05 +0300

    Альтернативы использованию match с Result<T, E>.

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -14,18 +14,30 @@ fn main() {
     use std::fs::File;
     use std::io::ErrorKind;
 
-    let greeting_file_result = File::open("hello.txt");
+    // let greeting_file_result = File::open("hello.txt");
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {e:?}"),
-            },
-            _ => {
-                panic!("Problem opening the file: {error:?}");
-            }
-        },
-    };
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {e:?}"),
+    //         },
+    //         _ => {
+    //             panic!("Problem opening the file: {error:?}");
+    //         }
+    //     },
+    // };
+
+    // Альтернативы использованию match с Result<T, E>
+
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
 }
