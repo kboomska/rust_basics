dd55cef27b89d778b12ea28a05dfc7bfe14851a3
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-10 11:49:31 +0300

    Исправимые ошибки с Result.

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -7,5 +7,16 @@ fn main() {
 
     let v = vec![1, 2, 3];
 
-    v[99];
+    // v[99]; // Ошибка
+
+    // Исправимые ошибки с Result
+
+    use std::fs::File;
+
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {error:?}"),
+    };
 }
