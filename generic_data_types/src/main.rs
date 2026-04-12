36877ca5709e67231819373d518a84b034fc1b90
Aleksey Kuzmin <kboomska@gmail.com>
2026-04-12 17:08:15 +0300

    Обобщённые типы данных в определении методов.

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -26,21 +26,21 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
 
 // Обобщённые типы данных в объявлении структур
 
-struct Point<T> {
-    x: T,
-    y: T,
-}
-struct AnotherPoint<T, U> {
-    x: T,
-    y: U,
-}
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+// struct AnotherPoint<T, U> {
+//     x: T,
+//     y: U,
+// }
 
-fn main() {
-    let integer = Point { x: 4, y: 10 };
-    let float = Point { x: 1.0, y: 10.0 };
-    // let wont_work = Point { x: 5, y: 4.0 }; // Ошибка!
-    let wont_work = AnotherPoint { x: 5, y: 4.0 };
-}
+// fn main() {
+//     let integer = Point { x: 4, y: 10 };
+//     let float = Point { x: 1.0, y: 10.0 };
+//     // let wont_work = Point { x: 5, y: 4.0 }; // Ошибка!
+//     let wont_work = AnotherPoint { x: 5, y: 4.0 };
+// }
 
 // Обобщённые типы данных в объявлении перечислений
 
@@ -52,4 +52,53 @@ enum Option<T> {
 enum Result<T, E> {
     Ok(T),
     Err(E),
+}
+
+// Обобщённые типы данных в определении методов
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+// Определяем метод для конкретного типа f32
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+// fn main() {
+//     let p = Point { x: 5, y: 10 };
+
+//     println!("p.x = {}", p.x());
+// }
+
+struct SomePoint<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> SomePoint<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: SomePoint<X2, Y2>) -> SomePoint<X1, Y2> {
+        SomePoint {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let p1 = SomePoint { x: 5, y: 10.4 };
+    let p2 = SomePoint { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y); // p3.x = 5, p3.y = c
 }
