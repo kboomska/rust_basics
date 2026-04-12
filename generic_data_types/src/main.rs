2ae806a8743b545740449ac340146ece55e6f22e
Aleksey Kuzmin <kboomska@gmail.com>
2026-04-12 16:15:17 +0300

    Обобщённые типы данных в объявлении структур.

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -12,14 +12,32 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     largest
 }
 
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {result}");
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {result}");
+// }
+
+// Обобщённые типы данных в объявлении структур
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+struct AnotherPoint<T, U> {
+    x: T,
+    y: U,
+}
+
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {result}");
+    let integer = Point { x: 4, y: 10 };
+    let float = Point { x: 1.0, y: 10.0 };
+    // let wont_work = Point { x: 5, y: 4.0 }; // Ошибка!
+    let wont_work = AnotherPoint { x: 5, y: 4.0 };
 }
