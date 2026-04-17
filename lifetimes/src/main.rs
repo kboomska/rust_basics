81c6cc42dada507598514f3274c4ae346a797466
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-17 16:13:32 +0300

    Определение времён жизни при объявлении структур.

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -36,15 +36,15 @@
 
 // Обобщённые времена жизни в функциях
 
-fn main() {
-    let string1 = String::from("long string is long");
-    {
-        let string2 = "xyz";
+// fn main() {
+//     let string1 = String::from("long string is long");
+//     {
+//         let string2 = "xyz";
 
-        let result = longest(string1.as_str(), string2);
-        println!("The longest string is {result}");
-    }
-}
+//         let result = longest(string1.as_str(), string2);
+//         println!("The longest string is {result}");
+//     }
+// }
 
 // Аннотации времени жизни в сигнатурах функций
 
@@ -59,3 +59,17 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 //     let result = String::from("really long string");
 //     result.as_str() // Ошибка!
 // }
+
+// Определение времён жизни при объявлении структур
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+}
