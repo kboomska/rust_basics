ad856808465291cfb78cdcdfbe1510cd5481b17f
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-20 12:04:05 +0300

    Проверка результатов с помощью макроса assert!.

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -6,14 +6,16 @@ pub fn add(left: u64, right: u64) -> u64 {
 mod tests {
     use super::*;
 
+    // Структура тестирующей функции
+
     #[test]
     fn exploration() {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
 
-    #[test]
-    fn another() {
-        panic!("Make this test fail")
-    }
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail")
+    // }
 }
