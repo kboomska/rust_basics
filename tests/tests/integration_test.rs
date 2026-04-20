64077d19d53992457759e98696bb576eee3c6d8a
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-20 17:30:10 +0300

    Подмодули в интеграционных тестах.

--- tests/tests/integration_test.rs
+++ tests/tests/integration_test.rs
@@ -8,3 +8,15 @@ fn it_equal_ten() {
     let return_result = sum(4, 6);
     assert_eq!(add_result, return_result);
 }
+
+// Подмодули в интеграционных тестах
+
+mod common;
+
+#[test]
+fn it_adds_two() {
+    common::setup();
+
+    let result = add_two(2);
+    assert_eq!(result, 4);
+}
