3907457ce26c211351503829c9f9ed4891bf1d86
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-23 13:55:28 +0300

    Типаж Iterator и метод next.

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -9,3 +9,25 @@ fn main() {
         println!("Got: {val}");
     }
 }
+
+// Типаж Iterator и метод next
+
+// pub trait Iterator {
+//     type Item;
+
+//     fn next(&mut self) -> Option<Self::Item>;
+
+//     // methods with default implementations elided
+// }
+
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
