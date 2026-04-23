c5c87be4183c404f66769a4491bbc07d671fa02d
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-04-23 11:56:53 +0300

    Выведение и аннотация типов замыкания.

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
@@ -49,4 +51,22 @@ fn main() {
         "The user with preference {:?} gets {:?}",
         user_pref2, giveaway2
     );
+
+    // Выведение и аннотация типов замыкания
+
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    // fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+    // let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    // let add_one_v3 = |x|             { x + 1 };
+    // let add_one_v4 = |x|               x + 1  ;
+
+    let example_closure = |x| x;
+
+    let s = example_closure(String::from("hello"));
+    // let n = example_closure(5); // Ошибка! Тип определен ранее String
 }
