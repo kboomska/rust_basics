a47f14ae4a92fac46cc1735d06ddb17b7fc6af4f
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-02 16:24:46 +0300

    Включение рекурсивных типов с помощью Boxes.

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,5 +1,29 @@
+// fn main() {
+//     // Хранение значения i32 в куче
+//     let b = Box::new(5);
+//     println!("b = {b}");
+// }
+
+// Включение рекурсивных типов с помощью Boxes
+
+// enum List {
+//     Cons(i32, List), // Рекурсивное определение вызывает ошибку.
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+
+// fn main() {
+//     let list = Cons(1, Cons(2, Cons(3, Nil)));
+// }
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
-    // Хранение значения i32 в куче
-    let b = Box::new(5);
-    println!("b = {b}");
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
