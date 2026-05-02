561cb9c2265e7235a372100e6a5b525f3167f78f
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-02 17:16:57 +0300

    Определение собственного умного указателя.

--- deref_example/src/main.rs
+++ deref_example/src/main.rs
@@ -1,8 +1,37 @@
+// fn main() {
+//     let x = 5;
+//     let y = Box::new(x);
+
+//     // Использование оператора разыменования с типом Box<i32>
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y); // Разыменование - переход от указателя Box<T> к значению
+// }
+
+// Определение собственного умного указателя
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// Трактование типа как ссылки реализуя типаж Deref
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
     let x = 5;
-    let y = Box::new(x);
+    let y = MyBox::new(x);
 
-    // Использование оператора разыменования с типом Box<i32>
     assert_eq!(5, x);
-    assert_eq!(5, *y); // Разыменование - переход от указателя Box<T> к значению
+    assert_eq!(5, *y); // *(y.deref())
 }
