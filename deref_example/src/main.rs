c4ab3b148bfba97f0f2df088d41d1b67de7dd64d
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-02 17:25:09 +0300

    Неявные разыменованные приведения ссылок в функциях и методах.

--- deref_example/src/main.rs
+++ deref_example/src/main.rs
@@ -28,10 +28,24 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y); // *(y.deref())
+
+    // Неявные разыменованные приведения с функциями и методами
+
+    let m = MyBox::new(String::from("Rust"));
+    // Преобразование &MyBox<String> в &String, а затем в &str, вызывая deref
+    hello(&m);
+
+    // Без реализации разыменованного приведения ссылок в Rust код выглядел бы
+    // иначе:
+    // hello(&(*m)[..]);
 }
