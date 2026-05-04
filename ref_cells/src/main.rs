9c82446c22c19f90f5eea6d6e73275c188260fab
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-05-04 12:55:38 +0300

    Наличие нескольких владельцев изменяемых данных путём объединения типов Rc<T> и RefCell<T>.

--- ref_cells/src/main.rs
+++ ref_cells/src/main.rs
@@ -19,9 +19,35 @@
 
 // Внутренняя изменяемость: изменяемое заимствование неизменяемого значения
 
-fn main() {
-    let x = 5;
-    // let y = &mut x; // Ошибка!
+// fn main() {
+//     let x = 5;
+//     // let y = &mut x; // Ошибка!
+// }
+
+// Наличие нескольких владельцев изменяемых данных путём объединения типов
+// Rc<T> и RefCell<T>
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
 }
 
-// Использование внутренней изменяемости: мок объекты
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
+}
