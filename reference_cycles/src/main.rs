5f070b399e2a3f071edbad63932232fb57e009c5
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-05-05 13:28:30 +0300

    Визуализация изменений в strong_count и weak_count.

--- reference_cycles/src/main.rs
+++ reference_cycles/src/main.rs
@@ -69,4 +69,47 @@ fn main() {
 
     println!("branch rc count {}", Rc::strong_count(&branch));
     println!("branch weak count {}", Rc::weak_count(&branch));
+
+    // Визуализация изменений в strong_count и weak_count
+
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
