4ac770b88a8e0ef0395fc5cd2d1b970738a59bd2
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-05-06 11:50:23 +0300

    Ожидание завершения работы всех потоков используя join.

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -2,15 +2,19 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    handle.join().unwrap(); // Сначала дожидается завершения порожденного потока
+
     for i in 1..5 {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+
+    // handle.join().unwrap(); // Приводит к чередованию вывода
 }
