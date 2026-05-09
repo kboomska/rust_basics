2fa434f790ef816e01f0e17819cd3a5e4f1633db
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-09 16:44:44 +0300

    Передача данных с помощью сообщений между потоками.

---
+++ message_passing/src/main.rs
@@ -0,0 +1,16 @@
+// Передача данных с помощью сообщений между потоками
+
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
+}
