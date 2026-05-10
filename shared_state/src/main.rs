20c08b192b67f0418aa35a5f4c30e1c7390a477b
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-10 21:12:27 +0300

    Разделение Mutex<T> между множеством потоков. Атомарный счётчик ссылок Arc<T>.

--- shared_state/src/main.rs
+++ shared_state/src/main.rs
@@ -1,12 +1,36 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+// fn main() {
+//     let m = Mutex::new(5);
+
+//     {
+//         let mut num = m.lock().unwrap();
+//         *num = 6;
+//     }
+
+//     println!("m = {m:?}");
+// }
+
+// Разделение Mutex<T> между множеством потоков
 
 fn main() {
-    let m = Mutex::new(5);
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
     }
 
-    println!("m = {m:?}");
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
