14f5c0368771885dc39fff99b5adfed18d8de16d
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-15 21:14:29 +0300

    Реализация переходов между состояниями в виде преобразований в другие типы.

--- blog_rust/src/main.rs
+++ blog_rust/src/main.rs
@@ -3,12 +3,11 @@ use blog_rust::Post;
 fn main() {
     let mut post = Post::new();
 
-    // post.add_text("I ate a salad for lunch");
-    // assert_eq!("", post.content());
+    post.add_text("I ate a salad for lunch today");
 
-    // post.request_review();
-    // assert_eq!("", post.content());
+    let post = post.request_review();
 
-    // post.approve();
-    // assert_eq!("I ate a salad for lunch today", post.content());
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
