407391ebd7c4028d1a866e2914675fe3c34a041e
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-16 21:31:47 +0300

    Цепочка await вызовов.

--- hello_async/src/main.rs
+++ hello_async/src/main.rs
@@ -1,11 +1,28 @@
 use trpl::Html;
 
 async fn page_title(url: &str) -> Option<String> {
-    let response = trpl::get(url).await;
-    let response_text = response.text().await;
+    // let response = trpl::get(url).await;
+    // let response_text = response.text().await;
+
+    // Chaining with the await keyword
+    let response_text = trpl::get(url).await.text().await;
+
     Html::parse(&response_text)
         .select_first("title")
         .map(|title| title.inner_html())
 }
+
+// Эквивалентное для компилятора написание функции page_title
+// use std::future::Future;
+// use trpl::Html;
+
+// fn page_title(url: &str) -> impl Future<Output = Option<String>> {
+//     async move {
+//         let text = trpl::get(url).await.text().await;
+//         Html::parse(&text)
+//             .select_first("title")
+//             .map(|title| title.inner_html())
+//     }
+// }
 
 fn main() {}
