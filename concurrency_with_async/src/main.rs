fdc2ec416cc498880f586d752ba146258c2e51f1
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-05-19 12:23:50 +0300

    Joining a Number of Futures with the join! Macro.

--- concurrency_with_async/src/main.rs
+++ concurrency_with_async/src/main.rs
@@ -77,10 +77,39 @@ fn main() {
     // Separating send and recv into their own async blocks and awaiting the
     // futures for those blocks
 
+    // trpl::block_on(async {
+    //     let (tx, mut rx) = trpl::channel();
+
+    //     let tx_fut = async move {
+    //         let vals = vec![
+    //             String::from("hi"),
+    //             String::from("from"),
+    //             String::from("the"),
+    //             String::from("future"),
+    //         ];
+
+    //         for val in vals {
+    //             tx.send(val).unwrap();
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //     let rx_fut = async {
+    //         while let Some(value) = rx.recv().await {
+    //             println!("received '{value}'");
+    //         }
+    //     };
+
+    //     trpl::join(tx_fut, rx_fut).await;
+    // });
+
+    // Joining a Number of Futures with the join! Macro
+
     trpl::block_on(async {
         let (tx, mut rx) = trpl::channel();
 
-        let tx_fut = async move {
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
@@ -89,7 +118,7 @@ fn main() {
             ];
 
             for val in vals {
-                tx.send(val).unwrap();
+                tx1.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
         };
@@ -100,6 +129,20 @@ fn main() {
             }
         };
 
-        trpl::join(tx_fut, rx_fut).await;
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join!(tx1_fut, tx_fut, rx_fut);
     });
 }
