0d620d960b321269c4a5789e3b767f8bc4688173
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-05-19 11:14:04 +0300

    Calling page_title for two URLs to see which returns first.

--- hello_async/src/main.rs
+++ hello_async/src/main.rs
@@ -1,15 +1,17 @@
-use trpl::Html;
+use trpl::{Either, Html};
 
-async fn page_title(url: &str) -> Option<String> {
+async fn page_title(url: &str) -> (&str, Option<String>) {
     // let response = trpl::get(url).await;
     // let response_text = response.text().await;
 
     // Chaining with the await keyword
     let response_text = trpl::get(url).await.text().await;
 
-    Html::parse(&response_text)
+    let title = Html::parse(&response_text)
         .select_first("title")
-        .map(|title| title.inner_html())
+        .map(|title| title.inner_html());
+
+    (url, title)
 }
 
 // Эквивалентное для компилятора написание функции page_title
@@ -29,10 +31,20 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     trpl::block_on(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} was {title}"),
-            None => println!("{url} had no title"),
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
+
+        let (url, maybe_title) =
+            match trpl::select(title_fut_1, title_fut_2).await {
+                Either::Left(left) => left,
+                Either::Right(right) => right,
+            };
+
+        println!("{url} returned first");
+
+        match maybe_title {
+            Some(title) => println!("Its page title was: {title}"),
+            None => println!("It had no title."),
         }
     })
 }
