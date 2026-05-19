eefd6cf239cb88d92a6ac969cd1fbf8cd8a01405
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-05-19 11:35:44 +0300

    Using trpl::join to await two anonymous futures.

--- concurrency_with_async/src/main.rs
+++ concurrency_with_async/src/main.rs
@@ -1,20 +1,39 @@
 use std::time::Duration;
 
 fn main() {
+    // trpl::block_on(async {
+    //     let handle = trpl::spawn_task(async {
+    //         for i in 1..10 {
+    //             println!("hi number {i} from the first task!");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     });
+
+    //     for i in 1..5 {
+    //         println!("hi number {i} from the second task!");
+    //         trpl::sleep(Duration::from_millis(500)).await;
+    //     }
+
+    //     // Дожидаемся завершения цикла из дополнительного потока.
+    //     handle.await.unwrap();
+    // });
+
     trpl::block_on(async {
-        let handle = trpl::spawn_task(async {
+        let fut1 = async {
             for i in 1..10 {
                 println!("hi number {i} from the first task!");
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        });
+        };
 
-        for i in 1..5 {
-            println!("hi number {i} from the second task!");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+        let fut2 = async {
+            for i in 1..5 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
 
-        // Дожидаемся завершения цикла из дополнительного потока.
-        handle.await.unwrap();
+        // Метод join дожидается завершения обоих Future
+        trpl::join(fut1, fut2).await;
     });
 }
