017eb191352aadf5b94daff52ede970879bbfb45
Aleksey Kuzmin <a.kuzmin@inmedikum.ru>
2026-05-20 10:54:31 +0300

    Building Our Own Async Abstractions.

--- more_futures/src/main.rs
+++ more_futures/src/main.rs
@@ -1,5 +1,7 @@
 use std::{thread, time::Duration};
 
+use trpl::Either;
+
 fn main() {
     // Calling the slow function to simulate slow operations
 
@@ -60,33 +62,59 @@ fn main() {
 
     // Using yield_now to let operations switch off making progress
 
+    // trpl::block_on(async {
+    //     let a = async {
+    //         println!("'a' started.");
+    //         slow("a", 30);
+    //         trpl::yield_now().await;
+    //         slow("a", 10);
+    //         trpl::yield_now().await;
+    //         slow("a", 20);
+    //         trpl::yield_now().await;
+    //         println!("'a' finished.");
+    //     };
+
+    //     let b = async {
+    //         println!("'b' started.");
+    //         slow("b", 75);
+    //         trpl::yield_now().await;
+    //         slow("b", 10);
+    //         trpl::yield_now().await;
+    //         slow("b", 15);
+    //         trpl::yield_now().await;
+    //         slow("b", 350);
+    //         trpl::yield_now().await;
+    //         println!("'b' finished.");
+    //     };
+
+    //     trpl::select(a, b).await;
+    // });
+
+    // Building Our Own Async Abstractions
+
     trpl::block_on(async {
-        let a = async {
-            println!("'a' started.");
-            slow("a", 30);
-            trpl::yield_now().await;
-            slow("a", 10);
-            trpl::yield_now().await;
-            slow("a", 20);
-            trpl::yield_now().await;
-            println!("'a' finished.");
+        let slow = async {
+            trpl::sleep(Duration::from_secs(5)).await;
+            "Finally finished"
         };
 
-        let b = async {
-            println!("'b' started.");
-            slow("b", 75);
-            trpl::yield_now().await;
-            slow("b", 10);
-            trpl::yield_now().await;
-            slow("b", 15);
-            trpl::yield_now().await;
-            slow("b", 350);
-            trpl::yield_now().await;
-            println!("'b' finished.");
-        };
-
-        trpl::select(a, b).await;
+        match timeout(slow, Duration::from_secs(2)).await {
+            Ok(message) => println!("Succeeded with '{message}'"),
+            Err(duration) => {
+                println!("Failed after {} seconds", duration.as_secs())
+            }
+        }
     });
+}
+
+async fn timeout<F: Future>(
+    future_to_try: F,
+    max_time: Duration,
+) -> Result<F::Output, Duration> {
+    match trpl::select(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }
 }
 
 fn slow(name: &str, ms: u64) {
