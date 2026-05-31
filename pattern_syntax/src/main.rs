9bf3861c528dbca7a6e9f7235f3c0985a6776726
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-31 17:46:21 +0300

    Игнорирование значения или его части с помощью шаблона _ (wildcard).

--- pattern_syntax/src/main.rs
+++ pattern_syntax/src/main.rs
@@ -130,4 +130,36 @@ fn main() {
     // Деструктуризация структур и кортежей
 
     let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    // Игнорирование всего значения с помощью шаблона _
+
+    fn foo(_: i32, y: i32) {
+        println!("This code only uses the y parameter: {y}");
+    }
+
+    foo(3, 4);
+
+    // Игнорирование частей значения с помощью вложенного _
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {setting_value:?}");
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}");
+        }
+    }
 }
