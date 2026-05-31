46255e1c3a8a06fbe34d95d97bf343306afb4581
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-31 14:00:33 +0300

    Синтаксис шаблонов: деструктуризация структуры.

--- pattern_syntax/src/main.rs
+++ pattern_syntax/src/main.rs
@@ -49,4 +49,29 @@ fn main() {
         'k'..='z' => println!("late ASCII letter"),
         _ => println!("something else"),
     }
+
+    // Деструктуризация структуры
+
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
 }
