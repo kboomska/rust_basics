58f4be27f9f00f27558c9a026a8528f3ed97581a
Aleksey Kuzmin <kboomska@gmail.com>
2026-05-31 16:44:04 +0300

    Синтаксис шаблонов: деструктуризация вложенных структур, перечислений и кортежей.

--- pattern_syntax/src/main.rs
+++ pattern_syntax/src/main.rs
@@ -74,4 +74,60 @@ fn main() {
             println!("On neither axis: ({x}, {y})");
         }
     }
+
+    // Деструктуризация перечислений
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+    }
+
+    // Деструктуризация вложенных структур и перечислений
+
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+
+    enum Command {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    let msg = Command::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Command::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+        Command::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h}, saturation {s}, value {v}");
+        }
+        _ => (),
+    }
+
+    // Деструктуризация структур и кортежей
+
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
 }
