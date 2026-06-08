7314425bf7f7072fb4048b9e717c4455f5b6edc9
Aleksey Kuzmin <kboomska@gmail.com>
2026-06-08 21:14:27 +0300

    Использование супер типажей для требования функциональности одного типажа в рамках другого типажа.

--- advanced_traits/src/main.rs
+++ advanced_traits/src/main.rs
@@ -49,6 +49,9 @@ fn main() {
 
     println!("A baby dog is called a {}", Dog::baby_name()); // Spot
     println!("A baby dog is called a {}", <Dog as Animal>::baby_name()); // puppy
+
+    let p = Point { x: 1, y: 3 };
+    p.outline_print();
 }
 
 trait Pilot {
@@ -94,5 +97,27 @@ impl Dog {
 impl Animal for Dog {
     fn baby_name() -> String {
         String::from("puppy")
+    }
+}
+
+use std::fmt;
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {output} *");
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+impl OutlinePrint for Point {}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
     }
 }
