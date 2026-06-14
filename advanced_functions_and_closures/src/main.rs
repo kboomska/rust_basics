ba9ea098d0373d2f33ee6e79102e84214208b3f2
Aleksey Kuzmin <kboomska@gmail.com>
2026-06-14 16:16:54 +0300

    Возврат замыканий.

--- advanced_functions_and_closures/src/main.rs
+++ advanced_functions_and_closures/src/main.rs
@@ -10,4 +10,26 @@ fn main() {
     let answer = do_twice(add_one, 5);
 
     println!("The answer is: {answer}");
+
+    let handlers = vec![returns_closure(), returns_initialized_closure(123)];
+    for handler in handlers {
+        let output = handler(5);
+        println!("{output}");
+    }
+}
+
+// fn returns_closure() -> impl Fn(i32) -> i32 {
+//     |x| x + 1
+// }
+
+// fn returns_initialized_closure(init: i32) -> impl Fn(i32) -> i32 {
+//     move |x| x + init
+// }
+
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
+fn returns_initialized_closure(init: i32) -> Box<dyn Fn(i32) -> i32> {
+    Box::new(move |x| x + init)
 }
